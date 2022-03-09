55bb5964898e7b904ccac28a5c540a2b553ba950
Didi Dodson <mdodson55@gmail.com>
2022-03-09 15:14:39 -0700

    Add update and spec

--- app/controllers/api/v1/fourteeners_controller.rb
+++ app/controllers/api/v1/fourteeners_controller.rb
@@ -4,12 +4,25 @@ class Api::V1::FourteenersController < ApplicationController
   end
 
   def show
-    render json: FourteenersSerializer.new(Fourteener.find(params[:id]))
+    if Fourteener.exists?(params[:id])
+      render json: FourteenersSerializer.new(Fourteener.find(params[:id]))
+    else
+      render json: { errors: { details: "There was an error finding this mountain." } }, status: 404
+    end
+  end
+
+  def update
+    mountain = Fourteener.update(params[:id], fourteener_params)
+    if mountain.save
+      render json: FourteenersSerializer.new(mountain)
+    else
+      render json: { errors: { details: "There was an error updating this mountain's details." } }, status: 400
+    end
   end
 
   private
 
   def fourteener_params
-    params.permit(:english_name)
+    params.permit(:arapahoe_name, :arapahoe_translation, :ute_name, :ute_translation, :elevation, :range, :county, :nearby_towns, :hiking_routes, :climate, :geology, :first_ascent, :history, :resources, :image, :map)
   end
 end
--- spec/requests/api/v1/fourteeners_request_spec.rb
+++ spec/requests/api/v1/fourteeners_request_spec.rb
@@ -38,4 +38,83 @@ RSpec.describe 'Fourteener API' do
     expect(mountain[:attributes][:english_name]).to_not eq(mtn2.english_name)
     expect(mountain[:attributes][:english_name]).to_not eq(mtn3.english_name)
   end
+
+  it 'sad path - finds one fourteener' do
+    get "/api/v1/fourteeners/10000000"
+
+    error = (JSON.parse(response.body, symbolize_names: true))[:errors][:details]
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    expect(error).to eq("There was an error finding this mountain.")
+  end
+
+  it 'happy path - updates a fourteener' do
+    mtn1 = create(:fourteener)
+
+    params = {
+      arapahoe_name: mtn1.arapahoe_name,
+      arapahoe_translation: mtn1.arapahoe_translation,
+      ute_name: mtn1.ute_name,
+      ute_translation: mtn1.ute_translation,
+      elevation: 14001,
+      range: mtn1.range,
+      county: 'Jefferson County',
+      nearby_towns: mtn1.nearby_towns,
+      hiking_routes: mtn1.hiking_routes,
+      climate: mtn1.climate,
+      geology: mtn1.geology,
+      first_ascent: "Jane Tanaka, 1927",
+      history: mtn1.history,
+      resources: mtn1.resources,
+      image: mtn1.image,
+      map: mtn1.map
+    }
+    header = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/fourteeners/#{mtn1.id}", headers: header, params: JSON.generate(params)
+
+    expect(response.status).to eq(200)
+
+    mountain = Fourteener.find_by(id: mtn1)
+
+    expect(mountain.elevation).to eq(14001)
+    expect(mountain.county).to eq("Jefferson County")
+    expect(mountain.first_ascent).to eq("Jane Tanaka, 1927")
+  end
+
+  it 'sad path - updates a fourteener' do
+    mtn1 = create(:fourteener)
+
+    params = {
+      arapahoe_name: mtn1.arapahoe_name,
+      arapahoe_translation: mtn1.arapahoe_translation,
+      ute_name: mtn1.ute_name,
+      ute_translation: mtn1.ute_translation,
+      elevation: "",
+      range: mtn1.range,
+      county: 'Jefferson County',
+      nearby_towns: mtn1.nearby_towns,
+      hiking_routes: mtn1.hiking_routes,
+      climate: mtn1.climate,
+      geology: mtn1.geology,
+      first_ascent: "Jane Tanaka, 1927",
+      history: mtn1.history,
+      resources: mtn1.resources,
+      image: mtn1.image,
+      map: mtn1.map
+    }
+
+    header = {"CONTENT_TYPE" => "application/json"}
+    
+    patch "/api/v1/fourteeners/#{mtn1.id}", headers: header, params: JSON.generate(params)
+
+    expect(response.status).to eq(400)
+
+    error = (JSON.parse(response.body, symbolize_names: true))[:errors][:details]
+
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(error).to eq("There was an error updating this mountain's details.")
+  end
 end
